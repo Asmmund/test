f119fe61f794dec4b3405a0ff8f3fb72af4e4e1c
Asmmund <antony.ermolenko@gmail.com>
2011-04-19 15:06:01 +0300

    class CRUD added

---
+++ libs/class/class.CRUD.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Elmor
+ * @copyright 2011
+ */
+    class CRUD
+    {
+        public function Create($row,$number,$hallid)
+        {
+            
+        }
+
+        public function Read($row,$number,$hallid)
+        {
+            
+        }
+
+        public function Update($row,$number,$hallid, $label)
+        {
+            
+        }
+        
+        public function Delete($row,$number,$hallid)
+        {
+            
+        }
+        
+
+    }
+?>
